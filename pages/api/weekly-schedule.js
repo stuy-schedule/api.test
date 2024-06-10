00e1e3aa4fb5f8300095e71bc7524e14c08395bc
Anthony Chen <50970130+achen318@users.noreply.github.com>
2024-06-09 20:41:51 -0400

    Update weekly-schedule.js

--- pages/api/weekly-schedule.js
+++ pages/api/weekly-schedule.js
@@ -27,7 +27,7 @@ export default async function handler(req, res) {
     scheduleType: 'week',
     days: [
       {
-        day: 'June 3, 2024',
+        day: 'June 10, 2024',
         bell: {
           scheduleType: 'regular',
           scheduleName: 'Regular Schedule',
@@ -144,19 +144,134 @@ export default async function handler(req, res) {
             }
           ]
         },
-        block: 'B',
+        block: 'A2',
         testing: 'Science Testing',
-        announcement: null
+        announcement: 'In-Class Finals'
       },
       {
-        day: 'June 4, 2024',
-        bell: null,
-        block: null,
-        testing: null,
-        announcement: 'Algebra I Regents\nGerman Exam'
+        day: 'June 11, 2024',
+        bell: {
+          scheduleType: 'regular',
+          scheduleName: 'Regular Schedule',
+          schedule: [
+            {
+              name: 'Before School',
+              startTime: '0:00',
+              duration: 420
+            },
+            {
+              name: 'Before Period 1',
+              startTime: '7:00',
+              duration: 60
+            },
+            {
+              name: 'Period 1',
+              startTime: '8:00',
+              duration: 41
+            },
+            {
+              name: 'Before Period 2',
+              startTime: '8:41',
+              duration: 4
+            },
+            {
+              name: 'Period 2',
+              startTime: '8:45',
+              duration: 41
+            },
+            {
+              name: 'Before Period 3',
+              startTime: '9:26',
+              duration: 5
+            },
+            {
+              name: 'Period 3',
+              startTime: '9:31',
+              duration: 44
+            },
+            {
+              name: 'Before Period 4',
+              startTime: '10:15',
+              duration: 5
+            },
+            {
+              name: 'Period 4',
+              startTime: '10:20',
+              duration: 41
+            },
+            {
+              name: 'Before Period 5',
+              startTime: '11:01',
+              duration: 5
+            },
+            {
+              name: 'Period 5',
+              startTime: '11:06',
+              duration: 41
+            },
+            {
+              name: 'Before Period 6',
+              startTime: '11:47',
+              duration: 5
+            },
+            {
+              name: 'Period 6',
+              startTime: '11:52',
+              duration: 41
+            },
+            {
+              name: 'Before Period 7',
+              startTime: '12:33',
+              duration: 5
+            },
+            {
+              name: 'Period 7',
+              startTime: '12:38',
+              duration: 41
+            },
+            {
+              name: 'Before Period 8',
+              startTime: '13:19',
+              duration: 5
+            },
+            {
+              name: 'Period 8',
+              startTime: '13:24',
+              duration: 41
+            },
+            {
+              name: 'Before Period 9',
+              startTime: '14:05',
+              duration: 4
+            },
+            {
+              name: 'Period 9',
+              startTime: '14:09',
+              duration: 41
+            },
+            {
+              name: 'Before Period 10',
+              startTime: '14:50',
+              duration: 4
+            },
+            {
+              name: 'Period 10',
+              startTime: '14:54',
+              duration: 41
+            },
+            {
+              name: 'After School',
+              startTime: '15:35',
+              duration: 504
+            }
+          ]
+        },
+        block: 'B2',
+        testing: 'Math, Music, Art & Technology Testing',
+        announcement: 'In-Class Finals'
       },
       {
-        day: 'June 5, 2024',
+        day: 'June 12, 2024',
         bell: {
           scheduleType: 'regular',
           scheduleName: 'Regular Schedule',
@@ -274,18 +389,11 @@ export default async function handler(req, res) {
           ]
         },
         block: 'A1',
-        testing: 'Math, Music, Art & Technology Testing',
-        announcement: null
+        testing: 'WL, ELA, CS & SS Testing',
+        announcement: 'In-Class Finals'
       },
       {
-        day: 'June 6, 2024',
-        bell: null,
-        block: null,
-        testing: null,
-        announcement: "Chancellor's Conference Day"
-      },
-      {
-        day: 'June 7, 2024',
+        day: 'June 13, 2024',
         bell: {
           scheduleType: 'regular',
           scheduleName: 'Regular Schedule',
@@ -403,10 +511,16 @@ export default async function handler(req, res) {
           ]
         },
         block: 'B1',
-        testing: 'WL, ELA, CS & SS Testing',
+        testing: null,
+        announcement: 'Makeup Exams\nEnd of MP3\nSenior Awards Night @ 5:30 pm'
+      },
+      {
+        day: 'June 14, 2024',
+        bell: null,
+        block: null,
+        testing: 'ELA Regents (Morning)\nLE Regents (Afternoon)',
         announcement: null
       }
     ]
   });
 }
-
